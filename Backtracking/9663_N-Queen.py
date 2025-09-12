c9a69f50f0dc133d60aa7a09ab120b7f413e761d
soju <rmrfroot001@gmail.com>
2025-09-12 10:32:42 +0900

    2025-09-12 2nd commit

--- Backtracking/9663_N-Queen.py
+++ Backtracking/9663_N-Queen.py
@@ -1,26 +1,29 @@
 import sys
 input = sys.stdin.readline
+# sys.setrecursionlimit(14)
 
 def dfs(board, checked_x, layer):
-    if layer == N:
-        return 1
     path = 0
 
     for row in range(1, layer):
-        px = checked_x[row] + (layer - row)
         mx = checked_x[row] - (layer - row)
+        px = checked_x[row] + (layer - row)
         if 0 < mx:
             board[layer][mx] = 1
         if px < N + 1:
             board[layer][px] = 1
 
     for i, x in enumerate(board[layer][1:]):
-        if x == 1: continue
+        if x == 1 or checked_x[layer - 1] == i + 1: continue
         else:
-            checked_x[layer] = i
+            if layer == N:
+                board[layer] = [-1 for _ in range(N + 1)]
+                return 1
+            checked_x[layer] = i + 1
             path += dfs(board, checked_x, layer + 1)
-            board[layer] = [-1 for _ in range(N + 1)]
-
+            checked_x[layer] = -1
+    board[layer] = [-1 for _ in range(N + 1)]
+    checked_x[layer] = -1
     return path
         
 if __name__ == "__main__":
@@ -32,7 +35,7 @@ if __name__ == "__main__":
         chessboard = [[-1] * (N + 1) for _ in range(N + 1)]
         chessboard[1][x] = 1
         checked_x = [-1 for _ in range(N + 1)]
-        checked_x[x] = 1
+        checked_x[x] = x
         layer = 1
         
         path += dfs(chessboard, checked_x, layer + 1)
